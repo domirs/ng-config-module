b05fa57f8ec4dbfec2296e38d4971c10d9c540af
Dominik Forster <dominik.forster@raiffeisen.ch>
2020-03-05 10:03:01 +0100

    update to angular 9

--- projects/config/karma.conf.js
+++ projects/config/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -31,8 +31,8 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
+        flags: ['--no-sandbox']
+      }
     }
   });
 };
